2dbcc2bd5cf8dad6242f1637da271f82a7b25ce5
stearm210 <pyzxWoo@163.com>
2025-01-07 15:51:53 +0800

    appa端文章微服务

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,8 +1,19 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
 
 public interface ApArticleService extends IService<ApArticle> {
-
+     /*
+      * @Title: load
+      * @Author: pyzxW
+      * @Date: 2025-01-07 15:34:30
+      * @Params: type 1 加载更多 2 加载最新
+      * @Return: null
+      * @Description: 加载文章列表
+      */
+//    业务层用于接收持久层传回来的参数(文章的list)，并且转换为controller需要进行返回的参数，也就是ResponseResult
+    public ResponseResult load(ArticleHomeDto dto, Short type);
 }
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -1,5 +1,18 @@
 package com.heima.article.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.article.service.ApArticleService;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @BelongsProject: heima-leadnews
  * @BelongsPackage: com.heima.article.service.impl
@@ -8,5 +21,27 @@ package com.heima.article.service.impl;
  * @Description: TODO
  * @Version: 1.0
  */
-public class ApArticleServiceImpl {
+
+@Service
+@Transactional
+@Slf4j
+public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle> implements ApArticleService {
+
+    @Autowired
+    private ApArticleMapper apArticleMapper;
+     /*
+      * @Title: load
+      * @Author: pyzxW
+      * @Date: 2025-01-07 15:43:05
+      * @Params:  type 1 为加载更多  2 为加载最新
+      * @Return: null
+      * @Description:
+      */
+    @Override
+    public ResponseResult load(ArticleHomeDto dto, Short type) {
+        List<ApArticle> articleList = apArticleMapper.loadArticleList(dto, type);
+
+        //返回最终的结果，封装于ResponseResult
+        return ResponseResult.okResult(articleList);
+    }
 }
