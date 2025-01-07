137f631fad47291ba5c0637eeffb50a93cbe7927
stearm210 <pyzxWoo@163.com>
2025-01-07 15:18:37 +0800

    appa端文章微服务

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -0,0 +1,62 @@
+package com.heima.article.controller.v1;
+
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.article.controller.v1
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-07  15:10
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+@RestController
+@RequestMapping("/api/v1/article")
+public class ArticleHomeController {
+
+     /*
+      * @Title: load
+      * @Author: pyzxW
+      * @Date: 2025-01-07 15:16:12
+      * @Params:
+      * @Return: null
+      * @Description: 加载首页
+      */
+    @PostMapping("/load")
+    public ResponseResult load(@RequestBody ArticleHomeDto dto){
+        return null;
+    }
+
+      /*
+       * @Title: loadmore
+       * @Author: pyzxW
+       * @Date: 2025-01-07 15:17:57
+       * @Params:
+       * @Return: null
+       * @Description: 加载更多
+       */
+    @PostMapping("/loadmore")
+    public ResponseResult loadmore(@RequestBody ArticleHomeDto dto){
+        return null;
+    }
+
+      /*
+       * @Title: loadnew
+       * @Author: pyzxW
+       * @Date: 2025-01-07 15:18:07
+       * @Params:
+       * @Return: null
+       * @Description: 加载最新
+       */
+    @PostMapping("/loadnew")
+    public ResponseResult loadnew(@RequestBody ArticleHomeDto dto){
+        return null;
+    }
+
+}
