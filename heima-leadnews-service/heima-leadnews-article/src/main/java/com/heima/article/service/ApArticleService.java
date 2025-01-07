2a9d92645d1ec082dd10d97c9ae3173867569059
stearm210 <pyzxWoo@163.com>
2025-01-07 15:33:01 +0800

    appa端文章微服务

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -0,0 +1,8 @@
+package com.heima.article.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.pojos.ApArticle;
+
+public interface ApArticleService extends IService<ApArticle> {
+
+}
---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.heima.article.service.impl;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.article.service.impl
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-07  15:32
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class ApArticleServiceImpl {
+}
