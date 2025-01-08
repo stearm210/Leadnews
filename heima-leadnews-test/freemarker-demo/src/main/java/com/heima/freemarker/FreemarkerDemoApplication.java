27be21a28ec91c2c7c3251cfd77487a9ae857cbd
stearm210 <pyzxWoo@163.com>
2025-01-08 14:54:19 +0800

    freemarker测试类

---
+++ heima-leadnews-test/freemarker-demo/src/main/java/com/heima/freemarker/FreemarkerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.heima.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.freemarker
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-08  14:51
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+public class FreemarkerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerDemoApplication.class,args);
+    }
+}
