21b1651682e465ef4c140518385cae4c52a91121
stearm210 <pyzxWoo@163.com>
2025-01-10 16:05:32 +0800

    minio上传文件

---
+++ heima-leadnews-service/heima-leadnews-article/src/test/java/com/heima/article/test/ArticleFreemarkerTest.java
@@ -0,0 +1,12 @@
+package com.heima.article.test;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.article.test
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-10  16:05
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class ArticleFreemarkerTest {
+}
--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -48,35 +48,33 @@ public class MinIOTest {
       * @Return: null
       * @Description: 上传到对应的文件夹内
       */
-    @Test
-    public void test() throws FileNotFoundException {
-        //上传文件，通过封装好的模版进行调用
-       FileInputStream fileInputStream = new FileInputStream("E:\\java study\\knowledge_picture\\Test_output\\list.html");
-       String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
-       System.out.println(path);
-    }
-
-
-
-//    public static void main(String[] args) {
-//        try {
-//            //需要上传的文件路径
-//            FileInputStream fileInputStream = new FileInputStream("E:\\java study\\knowledge_picture\\Test_output\\list.html");
-//
-//            //1.获取minio的连接信息，创建一个minio的客户端
-//            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
-//            //2.上传
-//            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-//                    .object("list.html")//文件名称
-//                    .contentType("text/html")//文件类型
-//                    .bucket("leadnews")//已经创建的桶名称，与minio界面中创建的一致
-//                    .stream(fileInputStream, fileInputStream.available(), -1).build();
-//            minioClient.putObject(putObjectArgs);
-//
-//            //访问路径
-//           System.out.println("http://192.168.200.130:9000/leadnews/list.html");
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
+//    @Test
+//    public void test() throws FileNotFoundException {
+//        //上传文件，通过封装好的模版进行调用
+//       FileInputStream fileInputStream = new FileInputStream("E:\\java study\\java资料\\黑马头条\\day02\\模板文件\\plugins\\css\\index.css");
+//       String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
+//       System.out.println(path);
 //    }
+
+    public static void main(String[] args) {
+        try {
+            //需要上传的文件路径
+            FileInputStream fileInputStream = new FileInputStream("E:\\java study\\java资料\\黑马头条\\day02\\模板文件\\plugins\\css\\index.css");
+
+            //1.获取minio的连接信息，创建一个minio的客户端
+            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
+            //2.上传
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("plugins/css/index.css")//文件名称
+                    .contentType("text/css")//文件类型
+                    .bucket("leadnews")//已经创建的桶名称，与minio界面中创建的一致
+                    .stream(fileInputStream, fileInputStream.available(), -1).build();
+            minioClient.putObject(putObjectArgs);
+
+            //访问路径
+           //System.out.println("http://192.168.200.130:9000/leadnews/list.html");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
