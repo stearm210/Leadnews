e7bfad7a4262a2f6c854f6150fc64354097b9d9e
stearm210 <pyzxWoo@163.com>
2025-01-11 20:49:26 +0800

    自媒体端开发

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
@@ -1,6 +1,8 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.wemedia.service.WmMaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1/material")
 public class WmMaterialController {
+    @Autowired
+    private WmMaterialService wmMaterialService;
 
     //上传图片至服务器
     @PostMapping("/upload_picture")
     public ResponseResult uploadPicture(MultipartFile multipartFile){
-        return null;
+        return wmMaterialService.uploadPicture(multipartFile);
     }
 }
