90c44330b967adade3ee0de57ab7e9853856fc75
stearm210 <pyzxWoo@163.com>
2025-01-11 19:55:07 +0800

    自媒体端开发

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/config/WebMvcConfig.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.heima.wemedia.config;
 
+import com.heima.wemedia.interceptor.WmTokenInterceptor;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.bouncycastle.pqc.crypto.newhope.NHOtherInfoGenerator;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +18,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+     /*
+      * @Title: addInterceptors
+      * @Author: pyzxW
+      * @Date: 2025-01-11 19:54:08
+      * @Params:
+      * @Return: null
+      * @Description: 配置使拦截器生效，拦截所有的请求
+      */
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-
+        //配置使拦截器生效，拦截所有的请求
+        registry.addInterceptor(new WmTokenInterceptor()).addPathPatterns("/**");
     }
 }
