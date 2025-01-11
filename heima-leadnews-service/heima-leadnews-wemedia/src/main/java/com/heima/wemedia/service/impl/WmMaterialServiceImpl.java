69101530a034630b8e023619fec018e21319f26d
stearm210 <pyzxWoo@163.com>
2025-01-11 20:15:08 +0800

    自媒体端开发

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
@@ -1,18 +1,42 @@
 package com.heima.wemedia.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.wemedia.mapper.WmMaterialMapper;
 import com.heima.wemedia.service.WmMaterialService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Slf4j
 @Service
 @Transactional
 public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMaterial> implements WmMaterialService {
+     /*
+      * @Title: uploadPicture
+      * @Author: pyzxW
+      * @Date: 2025-01-11 20:11:01
+      * @Params:
+      * @Return: null
+      * @Description: 图片上传实现类
+      */
+    @Override
+    public ResponseResult uploadPicture(MultipartFile multipartFile){
+        //1.检查参数
+        if (multipartFile == null || multipartFile.getSize() == 0){
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //2.上传图片到minIO中
+
+        //3.保存到对应的数据库中
+
+        //4.返回结果
+
+    }
 
 
 }
