181e1d88e0dafa7c7cc76953d298807468d3fc4a
stearm210 <pyzxWoo@163.com>
2025-01-12 16:29:34 +0800

    频道列表查询开发

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
@@ -0,0 +1,26 @@
+package com.heima.wemedia.controller.v1;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class WmNewsController {
+     /*
+      * @Title: findAll
+      * @Author: pyzxW
+      * @Date: 2025-01-12 16:29:11
+      * @Params:  
+      * @Return: null
+      * @Description: 文章列表信息查询
+      */
+    @PostMapping("/list")
+    public ResponseResult findAll(@RequestBody WmNewsPageReqDto dto){
+
+        return  null;
+    }
+}
