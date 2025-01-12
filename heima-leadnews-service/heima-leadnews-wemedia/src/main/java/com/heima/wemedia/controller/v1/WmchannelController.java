fcf9461724a8eee82d1a0eec5fe93988ca5cee7e
stearm210 <pyzxWoo@163.com>
2025-01-12 16:10:53 +0800

    频道列表查询开发

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmchannelController.java
@@ -0,0 +1,34 @@
+package com.heima.wemedia.controller.v1;
+
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.wemedia.controller.v1
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-12  16:06
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+@RestController
+@RequestMapping("/api/v1/channel")
+public class WmchannelController {
+     /*
+      * @Title: findAll
+      * @Author: pyzxW
+      * @Date: 2025-01-12 16:07:42
+      * @Params:
+      * @Return: null
+      * @Description: 对频道列表进行查询
+      */
+
+    @GetMapping("/channels")
+    public ResponseResult findAll(){
+        return null;
+    }
+
+}
