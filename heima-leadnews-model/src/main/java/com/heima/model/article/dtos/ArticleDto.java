babbf78f5fecfa2d331a93e0e9f695a312147551
stearm210 <pyzxWoo@163.com>
2025-01-14 15:56:23 +0800

    自媒体文章之审核操作

---
+++ heima-leadnews-model/src/main/java/com/heima/model/article/dtos/ArticleDto.java
@@ -0,0 +1,16 @@
+package com.heima.model.article.dtos;
+
+import com.heima.model.article.pojos.ApArticle;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.model.article.dtos
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-14  15:51
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class ArticleDto extends ApArticle {
+    //文章内容需要被传输
+    private String content;
+}
