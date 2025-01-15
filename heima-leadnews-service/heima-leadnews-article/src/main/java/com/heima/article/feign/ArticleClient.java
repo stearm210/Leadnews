38c10f000d0632c7a43f25a32bfbe6d7a57f52f9
stearm210 <pyzxWoo@163.com>
2025-01-15 15:28:39 +0800

    自媒体文章之审核操作

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
@@ -0,0 +1,35 @@
+package com.heima.article.feign;
+
+import com.heima.apis.article.IArticleClient;
+import com.heima.model.article.dtos.ArticleDto;
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.article.feign
+ * @Author: yanhongwei
+ * @CreateTime: 2025-01-15  15:25
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+
+@RestController
+public class ArticleClient implements IArticleClient {
+     /*
+      * @Title: saveArticle
+      * @Author: pyzxW
+      * @Date: 2025-01-15 15:26:49
+      * @Params:
+      * @Return: null
+      * @Description: 实现feign接口
+      */
+    @PostMapping("api/v1/article/save")
+    @Override
+    public ResponseResult saveArticle(@RequestBody ArticleDto dto) {
+        return null;
+    }
+}
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,9 +1,11 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ApArticleService extends IService<ApArticle> {
      /*
@@ -16,4 +18,14 @@ public interface ApArticleService extends IService<ApArticle> {
       */
 //    业务层用于接收持久层传回来的参数(文章的list)，并且转换为controller需要进行返回的参数，也就是ResponseResult
     public ResponseResult load(ArticleHomeDto dto, Short type);
+
+     /*
+      * @Title: saveArticle
+      * @Author: pyzxW
+      * @Date: 2025-01-15 15:28:24
+      * @Params:
+      * @Return: null
+      * @Description:保存app端相关文章
+      */
+    public ResponseResult saveArticle( ArticleDto dto);
 }
