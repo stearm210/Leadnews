87899e67a923e3fd8bbae8534092eb9602380ad0
stearm210 <pyzxWoo@163.com>
2025-01-15 15:59:25 +0800

    自媒体文章之审核操作

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
@@ -1,8 +1,10 @@
 package com.heima.article.feign;
 
 import com.heima.apis.article.IArticleClient;
+import com.heima.article.service.ApArticleService;
 import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArticleClient implements IArticleClient {
+
+    @Autowired
+    private ApArticleService apArticleService;
      /*
       * @Title: saveArticle
       * @Author: pyzxW
       * @Date: 2025-01-15 15:26:49
       * @Params:
       * @Return: null
-      * @Description: 实现feign接口
+      * @Description: 实现feign接口,远程调用？
       */
     @PostMapping("api/v1/article/save")
     @Override
     public ResponseResult saveArticle(@RequestBody ArticleDto dto) {
-        return null;
+        return apArticleService.saveArticle(dto);
     }
 }
