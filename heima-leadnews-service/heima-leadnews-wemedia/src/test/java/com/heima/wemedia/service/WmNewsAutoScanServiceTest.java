3ef2741253e6e5d24fa287032d3d2c1f884f8dd7
stearm210 <pyzxWoo@163.com>
2025-01-18 15:33:52 +0800

    自媒体文章之审核操作

--- heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/service/WmNewsAutoScanServiceTest.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/service/WmNewsAutoScanServiceTest.java
@@ -1,16 +1,17 @@
 package com.heima.wemedia.service;
 
-import org.junit.jupiter.api.Test;
+import com.heima.wemedia.WemediaApplication;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-@SpringBootTest(classes = WmNewsAutoScanService.class)
+@SpringBootTest(classes = WemediaApplication.class)
 @RunWith(SpringRunner.class)
-class WmNewsAutoScanServiceTest {
+public class WmNewsAutoScanServiceTest {
     @Autowired
     private WmNewsAutoScanService wmNewsAutoScanService;
 
