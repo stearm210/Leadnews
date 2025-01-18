dbfcae862570c323bcf3d71bf578da4529ceeb5f
stearm210 <pyzxWoo@163.com>
2025-01-18 20:50:06 +0800

    图片敏感信息识别

--- heima-leadnews-test/tess4j-demo/src/main/java/com/heima/tess4j/Application.java
+++ heima-leadnews-test/tess4j-demo/src/main/java/com/heima/tess4j/Application.java
@@ -22,17 +22,18 @@ public class Application {
       * @Params:
       * @Return: null
       * @Description: 识别图片中的文字
+      * 注意，对应的路径不能有中文
       */
     public static void main(String[] args) throws TesseractException {
         //创建实例
         ITesseract tesseract = new Tesseract();
         //设置字体库路径
-        tesseract.setDatapath("E:\\java study\\java资料\\黑马头条\\day04\\资料\\tessdata");
+        tesseract.setDatapath("E:\\tessdata");
         //设置语言，简体中文
         tesseract.setLanguage("chi_sim");
 
         //图片路径
-        File file = new File("E:\\java study\\java资料\\黑马头条\\day04\\资料\\picture.png");
+        File file = new File("E:\\1.png");
         //识别图片
         String result = tesseract.doOCR(file);
         System.out.println("识别的结果为："+result);
