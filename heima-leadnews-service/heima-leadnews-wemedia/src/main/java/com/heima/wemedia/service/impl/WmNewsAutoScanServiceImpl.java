d62fda837d895848f26e3895352608a857c11de5
stearm210 <pyzxWoo@163.com>
2025-01-18 16:34:31 +0800

    服务降级操作

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,6 +49,7 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
       * @Description: 自媒体文章审核
       */
     @Override
+    @Async //表明当前方法是一个异步的调用
     public void autoScanWmNews(Integer id) {
         //1.查询自媒体文章
         WmNews wmNews = wmNewsMapper.selectById(id);
