4896ebaef9e8fd4d13e0e4ec469d0a3592a54527
stearm210 <pyzxWoo@163.com>
2025-01-20 16:00:12 +0800

    添加延迟任务的实现

--- heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/Impl/TaskServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/Impl/TaskServiceImpl.java
@@ -1,10 +1,19 @@
 package com.heima.schedule.service.Impl;
 
+import com.heima.common.constants.ScheduleConstants;
 import com.heima.model.schedule.dtos.Task;
+import com.heima.model.schedule.pojos.Taskinfo;
+import com.heima.model.schedule.pojos.TaskinfoLogs;
+import com.heima.schedule.mapper.TaskinfoLogsMapper;
+import com.heima.schedule.mapper.TaskinfoMapper;
 import com.heima.schedule.service.TaskService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * @BelongsProject: heima-leadnews
@@ -28,7 +37,8 @@ public class TaskServiceImpl implements TaskService {
       */
     @Override
     public long addTask(Task task) {
-        //1.添加任务到数据库中
+        //1.添加任务到数据库中,抽出一个方法函数
+        boolean success = addTaskToDb(task);
 
         //2.添加任务到redis中
         //2.1 如果任务的执行时间小于等于当前时间，存入list
@@ -39,5 +49,46 @@ public class TaskServiceImpl implements TaskService {
         //这表示是延迟任务
 
         return 0;
+    }
+
+    @Autowired
+    private TaskinfoMapper taskinfoMapper;
+
+    @Autowired
+    private TaskinfoLogsMapper taskinfoLogsMapper;
+     /*
+      * @Title: addTaskToDb
+      * @Author: pyzxW
+      * @Date: 2025-01-20 15:46:36
+      * @Params:
+      * @Return: null
+      * @Description: 添加任务到数据库中,抽出一个方法函数
+      */
+    private boolean addTaskToDb(Task task){
+        boolean flag = false;
+        try{
+            //保存任务表
+            Taskinfo taskinfo = new Taskinfo();
+            //数据库参数批量导入，从task表中导入到taskinfo表中
+            BeanUtils.copyProperties(task,taskinfo);
+            //更变执行时间类型，方便时间从task表中导入到taskinfo表中
+            taskinfo.setExecuteTime(new Date(task.getExecuteTime()));
+            taskinfoMapper.insert(taskinfo);
+
+            //保存任务日志数据
+            TaskinfoLogs taskinfoLogs = new TaskinfoLogs();
+            //从taskinfo表中导入数据到taskinfoLogs表中
+            BeanUtils.copyProperties(taskinfo,taskinfoLogs);
+            //设置乐观锁的版本号
+            taskinfoLogs.setVersion(1);
+            //设置初始化状态
+            taskinfoLogs.setStatus(ScheduleConstants.SCHEDULED);
+            taskinfoLogsMapper.insert(taskinfoLogs);
+            //执行成功之后再次设置为true
+            flag = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
     }
 }
