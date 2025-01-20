133a7f89e3dee0f02f8efebd253aac242b0182c6
stearm210 <pyzxWoo@163.com>
2025-01-20 15:29:43 +0800

    使用redis实现远程调用

--- heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/test/RedisTest.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/test/RedisTest.java
@@ -1,10 +1,14 @@
 package com.heima.schedule.test;
 
+import com.heima.common.redis.CacheService;
 import com.heima.schedule.ScheduleApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
 
 /**
  * @BelongsProject: heima-leadnews
@@ -18,12 +22,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = ScheduleApplication.class)
 @RunWith(SpringRunner.class)
 public class RedisTest {
+    @Autowired
+    private CacheService cacheService;
+
     @Test
     public void testList(){
+        //list作为实时处理添加元素
+        //cacheService.lLeftPush("list_001","hello,redis");
+
+        //在list的右边获取元素，并删除
+        //String list_001 = cacheService.lRightPop("list_001");
+        //System.out.println(list_001);
     }
 
     @Test
     public void testZset(){
+        //添加数据到zset中  分值
+//        cacheService.zAdd("zset_key_001","hello zset 001",1000);
+//        cacheService.zAdd("zset_key_001","hello zset 002",8888);
+//        cacheService.zAdd("zset_key_001","hello zset 003",7777);
+//        cacheService.zAdd("zset_key_001","hello zset 004",999999);
 
+        //按照分值获取数据
+        //查前面3条数据
+        Set<String> zset_key_001 = cacheService.zRangeByScore("zset_key_001", 0, 8888);
+        System.out.println(zset_key_001);
     }
 }
