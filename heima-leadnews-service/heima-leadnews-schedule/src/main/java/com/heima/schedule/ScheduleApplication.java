c56796c5d951db99928097d0aa8a731d2e8b718d
stearm210 <pyzxWoo@163.com>
2025-01-20 15:06:19 +0800

    使用redis实现远程调用

--- heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
@@ -1,8 +1,11 @@
 package com.heima.schedule;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,5 +14,25 @@ public class ScheduleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class,args);
+    }
+
+ /*
+  * @Title:
+  * @Author: pyzxW
+  * @Date: 2025-01-20 15:05:38
+  * @Params:
+  * @Return: null
+  * @Description: 如果你想实现分页操作，也可以在这里添加对应的mybatis-plus操作进行分页代码编写
+  */
+
+    /**
+     * mybatis-plus乐观锁支持
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor optimisticLockerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
     }
 }
