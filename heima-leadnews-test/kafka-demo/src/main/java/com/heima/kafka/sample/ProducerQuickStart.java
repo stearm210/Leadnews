79a3d80bb21bd864a6f4aecaf2145411bd38742b
stearm210 <pyzxWoo@163.com>
2025-02-11 15:42:36 +0800

    kafka操作

---
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ProducerQuickStart.java
@@ -0,0 +1,12 @@
+package com.heima.kafka.sample;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.kafka.sample
+ * @Author: yanhongwei
+ * @CreateTime: 2025-02-11  15:41
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class ProducerQuickStart {
+}
