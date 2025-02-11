c5b3867f6955fc9fcdc4a077efadbd22d6cf78f1
stearm210 <pyzxWoo@163.com>
2025-02-11 16:14:36 +0800

    kafka操作

--- heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ConsumerQuickStart.java
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ConsumerQuickStart.java
@@ -1,5 +1,14 @@
 package com.heima.kafka.sample;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 /**
  * @BelongsProject: heima-leadnews
  * @BelongsPackage: com.heima.kafka.sample
@@ -8,8 +17,33 @@ package com.heima.kafka.sample;
  * @Description: TODO
  * @Version: 1.0
  */
+//消费者操作
 public class ConsumerQuickStart {
     public static void main(String[] args) {
+        //1.kafka链接配置信息
+        Properties properties = new Properties();
+        //设置kafka的连接地址
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.200.130:9092");
+        //消息的反序列化器
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        //消费者组
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
 
+        //2.创建kafka消费者对象
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //3.订阅topic
+        consumer.subscribe(Collections.singletonList("topic-first"));
+
+        //4.拉取消息
+        //处于一种监听的状态，防止线程死去
+        while (true) {
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.key());
+                System.out.println(consumerRecord.value());
+            }
+        }
     }
 }
