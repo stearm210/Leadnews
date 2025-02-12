8026ae968d769f00519e21723209737631389f2c
stearm210 <pyzxWoo@163.com>
2025-02-12 14:51:26 +0800

    kafka操作

--- heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ConsumerQuickStart.java
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ConsumerQuickStart.java
@@ -10,40 +10,37 @@ import java.util.Collections;
 import java.util.Properties;
 
 /**
- * @BelongsProject: heima-leadnews
- * @BelongsPackage: com.heima.kafka.sample
- * @Author: yanhongwei
- * @CreateTime: 2025-02-11  15:41
- * @Description: TODO
- * @Version: 1.0
+ * 消费者
  */
-//消费者操作
 public class ConsumerQuickStart {
+
     public static void main(String[] args) {
-        //1.kafka链接配置信息
+        //1.添加kafka的配置信息
         Properties properties = new Properties();
-        //设置kafka的连接地址
+        //kafka的连接地址
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.200.130:9092");
+        //消费者组
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
         //消息的反序列化器
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        //消费者组
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
 
-        //2.创建kafka消费者对象
+        //2.消费者对象
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
-        //3.订阅topic
-        consumer.subscribe(Collections.singletonList("topic-first"));
+        //3.订阅主题
+        consumer.subscribe(Collections.singletonList("itheima-topic"));
 
-        //4.拉取消息
-        //处于一种监听的状态，防止线程死去
+        //当前线程一直处于监听状态
         while (true) {
+            //4.获取消息
             ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
             for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
                 System.out.println(consumerRecord.key());
                 System.out.println(consumerRecord.value());
             }
         }
+
     }
+
 }
--- heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ProducerQuickStart.java
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/sample/ProducerQuickStart.java
@@ -7,39 +7,33 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.util.Properties;
 
 /**
- * @BelongsProject: heima-leadnews
- * @BelongsPackage: com.heima.kafka.sample
- * @Author: yanhongwei
- * @CreateTime: 2025-02-11  15:41
- * @Description: TODO
- * @Version: 1.0
+ * 生产者
  */
- /*
-  * @Title:
-  * @Author: pyzxW
-  * @Date: 2025-02-11 15:43:11
-  * @Params:
-  * @Return: null
-  * @Description: kafka生产者操作
-  */
 public class ProducerQuickStart {
+
     public static void main(String[] args) {
-        //1.kafka链接配置信息
+        //1.kafka的配置信息
         Properties properties = new Properties();
-        //设置kafka的连接地址
+        //kafka的连接地址
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.200.130:9092");
-        //key和value的序列化类
+        //发送失败，失败的重试次数
+        properties.put(ProducerConfig.RETRIES_CONFIG,5);
+        //消息key的序列化器
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        //消息value的序列化器
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
 
-        //2.创建kafka生产者对象
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
-        //3.发送消息
-        //对应的参数分别为：topic,key,value
+        //2.生产者对象
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        //封装发送的消息
         ProducerRecord<String,String> record = new ProducerRecord<String, String>("itheima-topic","100001","hello kafka");
+
+        //3.发送消息
         producer.send(record);
 
-        //4.关闭消息通道。必须关闭，否则消息发送不成功
+        //4.关闭消息通道，必须关闭，否则消息发送不成功
         producer.close();
     }
+
 }
