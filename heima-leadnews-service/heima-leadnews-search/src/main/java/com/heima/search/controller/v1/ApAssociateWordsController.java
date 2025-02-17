efbb7f8f282e51abf0a629d9e0c472a63884f0b6
stearm210 <pyzxWoo@163.com>
2025-02-17 15:15:57 +0800

    删除历史记录操作

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApAssociateWordsController.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApAssociateWordsController.java
@@ -2,6 +2,8 @@ package com.heima.search.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.search.dtos.UserSearchDto;
+import com.heima.search.service.ApAssociateWordsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/associate")
 public class ApAssociateWordsController {
+    @Autowired
+    private ApAssociateWordsService apAssociateWordsService;
      /*
       * @Title: search
       * @Author: pyzxW
@@ -29,6 +33,6 @@ public class ApAssociateWordsController {
       */
     @PostMapping("/search")
     public ResponseResult search(@RequestBody UserSearchDto userSearchDto) {
-        return null;
+        return apAssociateWordsService.findAssociate(userSearchDto);
     }
 }
