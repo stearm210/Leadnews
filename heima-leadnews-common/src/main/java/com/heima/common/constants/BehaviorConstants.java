6d4f8f0b6314cd90c3d3ca8c5a46c32e1e8b64b9
stearm210 <pyzxWoo@163.com>
2025-02-18 15:50:54 +0800

    序列化与反序列化

---
+++ heima-leadnews-common/src/main/java/com/heima/common/constants/BehaviorConstants.java
@@ -0,0 +1,19 @@
+package com.heima.common.constants;
+
+ /*
+  * @Title:
+  * @Author: pyzxW
+  * @Date: 2025-02-18 15:50:47
+  * @Params:
+  * @Return: null
+  * @Description: 在common模块下新增常量类 - BehaviorConstants
+  */
+public class BehaviorConstants {
+ 
+    public static final String LIKE_BEHAVIOR="LIKE-BEHAVIOR-";
+    public static final String UN_LIKE_BEHAVIOR="UNLIKE-BEHAVIOR-";
+    public static final String COLLECTION_BEHAVIOR="COLLECTION-BEHAVIOR-";
+    public static final String READ_BEHAVIOR="READ-BEHAVIOR-";
+    public static final String APUSER_FOLLOW_RELATION="APUSER-FOLLOW-";
+    public static final String APUSER_FANS_RELATION="APUSER-FANS-";
+}
