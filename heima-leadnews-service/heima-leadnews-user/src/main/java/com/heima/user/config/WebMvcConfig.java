0cb2c020ee92b9341f41d7a08942dda79714fc33
stearm210 <pyzxWoo@163.com>
2025-02-19 15:33:41 +0800

    拦截器操作

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/config/WebMvcConfig.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/config/WebMvcConfig.java
@@ -13,8 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Description: TODO
  * @Version: 1.0
  */
+//这个注解表明该类是一个Spring配置类。
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+     /*
+      * @Title: addInterceptors
+      * @Author: pyzxW
+      * @Date: 2025-02-19 15:33:07
+      * @Params:
+      * @Return: null
+      * @Description: 代码的主要作用是通过拦截器（HandlerInterceptor）来对所有请求（/**）进行拦截处理。
+      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AppTokenInterceptor()).addPathPatterns("/**");
