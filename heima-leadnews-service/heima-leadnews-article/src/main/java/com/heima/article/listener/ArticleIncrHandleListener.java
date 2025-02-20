9c26a6f52c040c14e9c5bbbbc64178fc6626ce07
stearm210 <pyzxWoo@163.com>
2025-02-20 14:03:38 +0800

    用户之点赞操作

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/listener/ArticleIncrHandleListener.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/listener/ArticleIncrHandleListener.java
@@ -16,7 +16,15 @@ import org.springframework.stereotype.Component;
 public class ArticleIncrHandleListener {
     @Autowired
     private ApArticleService apArticleService;
- 
+
+     /*
+      * @Title: onMessage
+      * @Author: pyzxW
+      * @Date: 2025-02-20 14:03:26
+      * @Params:
+      * @Return: null
+      * @Description: 文章分值更新
+      */
     @KafkaListener(topics = HotArticleConstants.HOT_ARTICLE_INCR_HANDLE_TOPIC)
     public void onMessage(String msg) {
         if(StringUtils.isNotBlank(msg)) {
