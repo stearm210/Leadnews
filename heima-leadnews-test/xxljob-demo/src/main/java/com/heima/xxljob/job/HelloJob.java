0de1d8e3072e4bca6739f8f7f6bba9c7840c8e7d
stearm210 <pyzxWoo@163.com>
2025-02-22 19:46:58 +0800

    分布式任务调度

--- heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/job/HelloJob.java
+++ heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/job/HelloJob.java
@@ -1,7 +1,11 @@
 package com.heima.xxljob.job;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @BelongsProject: heima-leadnews
@@ -26,5 +30,36 @@ public class HelloJob {
     @XxlJob("demoJobHandler")
     public void helloJob(){
         System.out.println("简单任务执行了。。。。");
+    }
+
+     /*
+      * @Title: shardingJobHandler
+      * @Author: pyzxW
+      * @Date: 2025-02-22 19:46:38
+      * @Params:
+      * @Return: null
+      * @Description: 分片之业务操作
+      */
+    @XxlJob("shardingJobHandler")
+    public void shardingJobHandler(){
+        //分片的参数
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+
+        //业务逻辑
+        List<Integer> list = getList();
+        for (Integer integer : list) {
+            if(integer % shardTotal == shardIndex){
+                System.out.println("当前第"+shardIndex+"分片执行了，任务项为："+integer);
+            }
+        }
+    }
+
+    public List<Integer> getList(){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
