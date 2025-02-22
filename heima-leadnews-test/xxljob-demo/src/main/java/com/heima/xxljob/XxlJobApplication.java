85517e26593043024edc6412aa032f8edfdc4d44
stearm210 <pyzxWoo@163.com>
2025-02-22 15:27:05 +0800

    分布式任务调度

---
+++ heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/XxlJobApplication.java
@@ -0,0 +1,20 @@
+package com.heima.xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: heima-leadnews
+ * @BelongsPackage: com.heima.xxljob
+ * @Author: yanhongwei
+ * @CreateTime: 2025-02-22  15:16
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+public class XxlJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class,args);
+    }
+}
